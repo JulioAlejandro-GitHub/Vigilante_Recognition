b5c97e70382ced76ff6f2665de76784fd61dfb84
JULIO ALEJANDRO MORALES GUTIERREZ <juliomoralesgutierrez@gmail.com>
2026-03-11 15:39:07 -0300

    url img recognition

--- Vigilante_v2_configurable.sql
+++ Vigilante_v2_configurable.sql
@@ -476,6 +476,19 @@ CREATE TABLE `alerta_enviada` (
     ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+
+
+ALTER TABLE recognition_event
+ADD COLUMN frame_image_url VARCHAR(255) DEFAULT NULL AFTER frame_img;
+
+ALTER TABLE recognition_face
+ADD COLUMN face_preview_img VARCHAR(255) DEFAULT NULL AFTER face_img,
+ADD COLUMN face_image_url VARCHAR(255) DEFAULT NULL AFTER face_preview_img,
+ADD COLUMN face_preview_url VARCHAR(255) DEFAULT NULL AFTER face_image_url;
+
+
+
 -- =========================================================
 -- 8) VISTAS ÚTILES PARA REPORTERÍA / UX
 -- =========================================================
